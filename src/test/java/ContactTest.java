0df9020588ac6dd06d1463d9e6b3eb157f68178d
Mustafa Alemi <mustafa@129.141.028.145.hva.nl>
2018-05-30 13:56:45 +0200

    Test

---
+++ src/test/java/ContactTest.java
@@ -0,0 +1,17 @@
+package
+import org.junit.Test;
+import eu.siacs.conversations.services.XmppConnectionService;
+import java.util.regex.Pattern;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class ContactTest {
+
+    @Test
+    public void contactValidator_AddContact_ReturnsTrue() {
+        assertTrue()
+        createContact;
+    }
+
+}
